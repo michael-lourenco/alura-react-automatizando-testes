81753c0e83c5455c9b9e94aa7a557be91579a189
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-18 16:36:21 -0300

    Teste com snapshot

---
+++ src/transacoes/Transacao.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Transacao from './Transacao';
+
+describe('Componente de trasação do extrato', () => {
+    it('O snapshot do component deve permanecer sempre o mesmo',() => {
+        const { container } = render(<Transacao 
+            data="08/09/2020"
+            tipo="saque"
+            valor="50.00"
+        />)
+        expect(container.firstChild).toMatchSnapshot();
+    })
+})
