af5bf163189ab721125430c75f3e074c9ba7ea5c
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-20 23:28:42 -0300

    Simulando o retorno de uma API

---
+++ src/AppIntegracoes.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import api from "./api";
+import App from './App';
+
+jest.mock('./api');
+
+describe('Requisições para a API', () => {
+    it('Exibir lista de tansações através da API', async () => {
+        api.listaTransacoes.mockResolvedValue([{
+            "valor": 10,
+            "transacao": "saque",
+            "data": "10/08/2020",
+            "id": 1
+          },
+          {
+            "transacao": "deposito",
+            "valor": "20",
+            "data": "26/09/2020",
+            "id": 2
+          }
+        ]);
+
+        render(<App />);
+        
+        expect(screen.getByTestId('transacoes').childen.length).toBe(2);
+    })
+})
